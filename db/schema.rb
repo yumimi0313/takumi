c7a378bececbe879c36a68cc7d08403e75945146
yumi yonemori <yumiyonemori@gmail.com>
2023-04-06 18:30:29 +0900

    メッセージ機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_04_064944) do
+ActiveRecord::Schema.define(version: 2023_04_06_075527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2023_04_04_064944) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.index ["user_id"], name: "index_candidates_on_user_id"
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id", "recipient_id"], name: "index_conversations_on_sender_id_and_recipient_id", unique: true
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
   end
 
   create_table "craftmen", force: :cascade do |t|
@@ -50,6 +60,17 @@ ActiveRecord::Schema.define(version: 2023_04_04_064944) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "read", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -102,6 +123,8 @@ ActiveRecord::Schema.define(version: 2023_04_04_064944) do
 
   add_foreign_key "candidates", "users"
   add_foreign_key "craftmen", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "products", "users"
   add_foreign_key "questions", "interviews"
 end
