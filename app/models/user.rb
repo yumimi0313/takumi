9fdc520ac5bd6f153e1d38cb46fe821a5d9fe1c5
yumi yonemori <yumiyonemori@gmail.com>
2023-04-20 14:19:19 +0900

    モデルバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,8 @@ class User < ApplicationRecord
   has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :email, presence: true, length: { maximum: 255 }
-  validates :encrypted_password, presence: true, length: { minimum: 6, maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6, maximum: 255 }
   validates :role, presence: true
   
   #指定のユーザをフォローする
