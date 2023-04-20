9490871fe754a8d8c625e38032664b88df1b24f1
yumi yonemori <yumiyonemori@gmail.com>
2023-04-20 14:07:16 +0900

    入力フォームプレースホルダー等修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many_attached :images
 
-  validates :name, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
 end
